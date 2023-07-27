7fa5f492c72593bfffe6a19a306b6ae0ab80c304
BatuSengezer <bsengezer@ku.edu.tr>
2023-07-27 15:39:28 +0200

    header file done

--- include/pipex.h
+++ include/pipex.h
@@ -1,8 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsengeze <bsengeze@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 15:06:33 by bsengeze          #+#    #+#             */
+/*   Updated: 2023/07/27 15:36:03 by bsengeze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include "../libft/libft.h"
+#include "libft.h"
 #include <sys/wait.h>
 #include <fcntl.h>
+
+void error_exit(void);
+void execute(char *argv, char **envp);
+char	*path_finder(char *cmd, char **envp);
 
 #endif
