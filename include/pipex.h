7da2938993de43d5a3935f610ca1db8fe51bc643
BatuSengezer <bsengezer@ku.edu.tr>
2023-07-30 18:40:33 +0200

    added bonus

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: bsengeze <bsengeze@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 15:06:33 by bsengeze          #+#    #+#             */
-/*   Updated: 2023/07/29 15:17:00 by bsengeze         ###   ########.fr       */
+/*   Updated: 2023/07/29 22:04:14 by bsengeze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@
 void	error_exit(void);
 void	execute(char *argv, char **envp);
 char	*path_finder(char *cmd, char **envp);
-char    **cmd_with_single_quotes_parser(char *argv);
+char	**cmd_with_single_quotes_parser(char *argv);
+int		get_next_line_pipex(char **line);
+int		open_file(char *argv, int i);
+void	wrong_input(void);
 
 #endif
