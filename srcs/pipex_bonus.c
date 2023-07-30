222a4f5c9f6860eb03b07dd03cc1733d49c71262
BatuSengezer <bsengezer@ku.edu.tr>
2023-07-30 18:41:40 +0200

    trying bonus

---
+++ srcs/pipex_bonus.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsengeze <bsengeze@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/29 17:03:39 by bsengeze          #+#    #+#             */
+/*   Updated: 2023/07/29 21:39:38 by bsengeze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+// Child process that create a fork and a pipe, put the output inside a pipe
+// and then close with the exec function. The main process will change his 
+// stdin for the pipe file descriptor.
+void	new_process(char *argv, char **envp)
+{
+	pid_t	pid;
+	int		fd[2];
+
+	if (pipe(fd) == -1)
+		error_exit();
+	pid = fork();
+	if (pid == -1)
+		error_exit();
+	if (pid == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		execute(argv, envp);
+	}
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		waitpid(pid, NULL, 0);
+	}
+}
+
+// Function who make a child process that will read from the stdin with
+// get_next_line until it find the limiter word and then put the output inside
+// a pipe. The main process will change his stdin for the pipe file descriptor.
+void	here_doc(char *limiter, int argc)
+{
+	pid_t	pid;
+	int		fd[2];
+	char	*line;
+
+	if (argc < 6)
+		wrong_input();
+	if (pipe(fd) == -1)
+		error_exit();
+	pid = fork();
+	if (pid == 0)
+	{
+		close(fd[0]);
+		while (get_next_line_pipex(&line))
+		{
+			if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0)
+				exit(EXIT_SUCCESS);
+			write(fd[1], line, ft_strlen(line));
+		}
+	}
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		wait(NULL);
+	}
+}
+
+// Main function that run the childs process with the right file descriptor
+// or display an error message if arguments are wrong. It will run here_doc
+// function if the "here_doc" string is find in argv[1]. 
+int	main(int argc, char **argv, char **envp)
+{
+	int	i;
+	int	filein;
+	int	fileout;
+
+	if (argc >= 5)
+	{
+		if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+		{
+			i = 3;
+			fileout = open_file(argv[argc - 1], 0);
+			here_doc(argv[2], argc);
+		}
+		else
+		{
+			i = 2;
+			filein = open_file(argv[1], 2);
+			dup2(filein, STDIN_FILENO);
+			fileout = open_file(argv[argc - 1], 1);
+		}
+		while (i < argc - 2)
+			new_process(argv[i++], envp);
+		dup2(fileout, STDOUT_FILENO);
+		execute(argv[argc - 2], envp);
+	}
+	wrong_input();
+}
